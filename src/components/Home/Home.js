efcf6d0af25cc251ef002eb33fa3c125eeb0cb0c
imranahmed1990 <imranmarin1990@gmail.com>
2022-04-03 21:31:00 +0600

    last commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,24 @@
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import img from '../../img/my-img.jpg';
 import './Home.css';
 const Home = () => {
     return (
-        <div className='container'>
-            <div className='text'>
-                <h1>WELCOME TO MY PORTFOLIO...</h1>
-                <p><small>I am professional js developer from last year..</small></p>
-                <button className='btn'>Download CV</button>
+        <div>
+            <div className='container'>
+                <div className='text'>
+                    <h1>WELCOME TO MY PORTFOLIO...</h1>
+                    <p><small>I am professional js developer from last year..</small></p>
+                    <button className='btn'>Download CV <FontAwesomeIcon incon={faArrowDown}></FontAwesomeIcon></button>
+                </div>
+                <div className='img-container'>
+                    <img src={img} alt="" />
+                </div>
             </div>
-            <div className='img-container'>
-                <img src={img} alt="" />
+
+            <div>
+                hello world
             </div>
         </div>
     );
